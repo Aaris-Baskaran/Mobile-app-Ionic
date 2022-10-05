c8112ab1b542800f874769981def7f1244a3bbe4
Morra-eng <66109245+Morra-eng@users.noreply.github.com>
2022-10-06 10:44:29 +1300

    added camera page

--- assignment3/src/app/app-routing.module.ts
+++ assignment3/src/app/app-routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
     path: 'sign-up',
     loadChildren: () => import('./sign-up/sign-up.module').then( m => m.SignUpPageModule)
   },
+  {
+    path: 'camera',
+    loadChildren: () => import('./camera/camera.module').then( m => m.CameraPageModule)
+  },
 
 ];
 
