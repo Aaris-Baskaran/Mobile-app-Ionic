dac61c2464c93fdf3c923c621f1935aa51630a2c
Morra-eng <66109245+Morra-eng@users.noreply.github.com>
2022-10-07 11:58:49 +1300

    changed home page by adding another ion-card

--- assignment3/src/app/home/home.page.ts
+++ assignment3/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/compat/auth'
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,13 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private router: Router,
+    public ngFBAuth: AngularFireAuth) { }
+
+ngOnInit() {
+}
+  goToCamera(){
+    this.router.navigate(['/camera']);
+  }
 
 }
